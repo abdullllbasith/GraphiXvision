a0a1c479457b4f6b0808f629eaf69f0bb88ef9bc
abdullllbasith <ab.unireg@gmail.com>
2024-12-03 19:18:12 +0530

    fixed some bugs

--- JS/script.js
+++ JS/script.js
@@ -214,7 +214,7 @@ $('.team-slider').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1, // Fixed key name
                 infinite: true,
                 dots: true,
